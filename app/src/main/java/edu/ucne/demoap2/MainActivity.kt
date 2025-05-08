29515f536d0654e0200f7a6de728b3d747225efd
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-05-07 20:42:15 -0400

    modificando el mainactivity

--- app/src/main/java/edu/ucne/demoap2/MainActivity.kt
+++ app/src/main/java/edu/ucne/demoap2/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import edu.ucne.demoap2.ui.theme.DemoAp2Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,31 +24,33 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             DemoAp2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
-                    Text(text = "Hello, Carlos", modifier = Modifier.padding(innerPadding))
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                extracted()
             }
         }
     }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+private fun extracted() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+        Column {
+
+            Text(text = "Hello, Carlos", modifier = Modifier.padding(innerPadding))
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = "Que tal estas mi hermano")
+
+        }
+
+
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     DemoAp2Theme {
-        Greeting("Carlos!")
+        extracted()
     }
 }
