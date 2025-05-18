70d4608a7c28d134e6700ade69dee352f104e161
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-05-18 15:40:51 -0400

    se creo el dao de tickets

---
+++ Registro_Tecnicos/app/src/main/java/edu/ucne/registro_tecnicos/data/local/dao/TicketDao.kt
@@ -0,0 +1,32 @@
+package edu.ucne.registro_tecnicos.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Update
+import androidx.room.Upsert
+import edu.ucne.registro_tecnicos.data.local.entities.TicketsEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TicketDao {
+    @Upsert
+    suspend fun save(ticket: TicketsEntity)
+    @Query("SELECT * FROM tickets WHERE tecnicoId = :tecnicoId")
+    fun getTicketsTecnico(tecnicoId: Int): Flow<List<TicketsEntity>>
+
+    @Query(
+        """
+            SELECT * FROM Tickets
+            WHERE ticketId = :id
+            LIMIT 1
+        """)
+    suspend fun find(id: Int): TicketsEntity?
+    @Update
+    suspend fun update(ticket: TicketsEntity)
+    @Delete
+    suspend fun delete(ticket: TicketsEntity)
+
+    @Query("SELECT * FROM Tickets")
+    fun getAll(): Flow<List<TicketsEntity>>
+}
