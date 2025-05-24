a01b4c78e74bea7098440ae0d2a92cb1864cd5af
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-05-24 12:36:19 -0400

    se creo el ticketresponse repository

---
+++ Registro_Tecnicos/app/src/main/java/edu/ucne/registro_tecnicos/data/repository/TicketResponseRepository.kt
@@ -0,0 +1,12 @@
+package edu.ucne.registro_tecnicos.data.repository
+
+import edu.ucne.registro_tecnicos.data.local.dao.TicketResponseDao
+import edu.ucne.registro_tecnicos.data.local.entities.TicketResponseEntity
+import kotlinx.coroutines.flow.Flow
+
+class TicketResponseRepository(private val dao: TicketResponseDao) {
+    suspend fun insert(response: TicketResponseEntity) = dao.insert(response)
+
+    fun getResponsesByTicket(ticketId: Int): Flow<List<TicketResponseEntity>> =
+        dao.getResponsesByTicket(ticketId)
+}
