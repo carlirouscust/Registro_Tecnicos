4fb60d330fb8ce40f7b1aaf4235994befa228acc
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-05-24 12:36:06 -0400

    se creo el ticketresponse entity

---
+++ Registro_Tecnicos/app/src/main/java/edu/ucne/registro_tecnicos/data/local/entities/TicketResponseEntity.kt
@@ -0,0 +1,16 @@
+package edu.ucne.registro_tecnicos.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "ticket_responses")
+
+data class TicketResponseEntity(
+    @PrimaryKey(autoGenerate = true)
+    val responseId: Int = 0,
+    val ticketId: Int,
+    val nombre: String,
+    val mensaje: String,
+    val fecha: String,
+    val tipoUsuario: String // "Operador" o "Owner"
+)
