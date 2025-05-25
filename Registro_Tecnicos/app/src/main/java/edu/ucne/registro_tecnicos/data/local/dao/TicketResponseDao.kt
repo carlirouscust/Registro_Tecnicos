d759e004599b6fe15aee5fd6ada953739b856d21
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-05-25 14:52:54 -0400

    se agrego el delete

--- Registro_Tecnicos/app/src/main/java/edu/ucne/registro_tecnicos/data/local/dao/TicketResponseDao.kt
+++ Registro_Tecnicos/app/src/main/java/edu/ucne/registro_tecnicos/data/local/dao/TicketResponseDao.kt
@@ -2,6 +2,7 @@ package edu.ucne.registro_tecnicos.data.local.dao
 
 import androidx.room.*
 import edu.ucne.registro_tecnicos.data.local.entities.TicketResponseEntity
+import edu.ucne.registro_tecnicos.data.local.entities.TicketsEntity
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -11,4 +12,8 @@ interface TicketResponseDao {
 
     @Query("SELECT * FROM ticket_responses WHERE ticketId = :ticketId ORDER BY responseId ASC")
     fun getResponsesByTicket(ticketId: Int): Flow<List<TicketResponseEntity>>
+
+    @Query("DELETE FROM ticket_responses WHERE ticketId = :ticketId")
+    suspend fun deleteResponsesByTicketId(ticketId: Int)
+
 }
