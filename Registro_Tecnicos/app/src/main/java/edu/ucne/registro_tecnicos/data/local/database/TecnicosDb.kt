93e83a7eecdee58fd536bd1b415b8b790c18b275
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-05-25 14:51:13 -0400

    se agrego el ticket response

--- Registro_Tecnicos/app/src/main/java/edu/ucne/registro_tecnicos/data/local/database/TecnicosDb.kt
+++ Registro_Tecnicos/app/src/main/java/edu/ucne/registro_tecnicos/data/local/database/TecnicosDb.kt
@@ -7,10 +7,12 @@ import androidx.room.RoomDatabase
 import androidx.sqlite.db.SupportSQLiteDatabase
 import edu.ucne.registro_tecnicos.data.local.dao.PrioridadDao
 import edu.ucne.registro_tecnicos.data.local.dao.TicketDao
-import edu.ucne.registro_tecnicos.data.local.entities.TecnicosEntity
-import edu.ucne.registro_tecnicos.data.local.entities.PrioridadEntity
+import edu.ucne.registro_tecnicos.data.local.dao.TicketResponseDao
 import edu.ucne.registro_tecnicos.data.local.dao.TecnicosDao
+import edu.ucne.registro_tecnicos.data.local.entities.PrioridadEntity
+import edu.ucne.registro_tecnicos.data.local.entities.TecnicosEntity
 import edu.ucne.registro_tecnicos.data.local.entities.TicketsEntity
+import edu.ucne.registro_tecnicos.data.local.entities.TicketResponseEntity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -19,16 +21,17 @@ import kotlinx.coroutines.launch
     entities = [
         TecnicosEntity::class,
         TicketsEntity::class,
-        PrioridadEntity::class
+        PrioridadEntity::class,
+        TicketResponseEntity::class
     ],
-    version = 11,
+    version = 12,
     exportSchema = false
 )
-
 abstract class TecnicosDb : RoomDatabase() {
     abstract fun tecnicosDao(): TecnicosDao
     abstract fun TicketDao(): TicketDao
     abstract fun PrioridadDao(): PrioridadDao
+    abstract fun ticketResponseDao(): TicketResponseDao
 
     companion object {
         @Volatile
@@ -41,10 +44,10 @@ abstract class TecnicosDb : RoomDatabase() {
                     TecnicosDb::class.java,
                     "tecnicos_db"
                 )
+                    .fallbackToDestructiveMigration()
                     .addCallback(object : RoomDatabase.Callback() {
                         override fun onCreate(db: SupportSQLiteDatabase) {
                             super.onCreate(db)
-                            // Lógica para insertar prioridades predeterminadas
                             CoroutineScope(Dispatchers.IO).launch {
                                 INSTANCE?.PrioridadDao()?.insertarPrioridades(
                                     listOf(
